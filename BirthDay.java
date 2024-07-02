c4d17eb7793d13c926ee5cace1b8f288d02fd569
Anushbabu0794 <anushbabukagitha07@gmail.com>
2024-07-02 17:52:48 +0000

    done

--- BirthDay.java
+++ BirthDay.java
@@ -1,69 +1,55 @@
-importjava.util.Scanner;
+import java.util.Scanner;
 
-publicclassBirthDay {
-    privateStringname;
-    privateintbirthMonth;
-    privateintbirthDay;
-    privateintbirthYear;
+public class BirthDay {
+    private String name;
+    private int birthMonth;
+    private int birthDay;
+    private int birthYear;
 
-    publicBirthDay(Stringname, intbirthMonth, intbirthDay, intbirthYear) {
-        this.name=name;
-        this.birthMonth=birthMonth;
-        this.birthDay=birthDay;
-        this.birthYear=birthYear;
+    public BirthDay(String name, int birthMonth, int birthDay, int birthYear) {
+        this.name = name;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+        this.birthYear = birthYear;
     }
 
-    publicStringgetUsaFormat() {
-        returnString.format("%d/%d/%d", birthMonth, birthDay, birthYear);
+    public void printBirthday(boolean isFriend) {
+        String prefix = isFriend ? "my friend " + name + "'s" : "my";
+        System.out.println("In the USA " + prefix + " birthday is: " + birthMonth + "/" + birthDay + "/" + birthYear);
+        System.out.println("In England " + prefix + " birthday is: " + birthDay + "/" + birthMonth + "/" + birthYear);
     }
 
-    publicStringgetUkFormat() {
-        returnString.format("%d/%d/%d", birthDay, birthMonth, birthYear);
-    }
-
-    publicstaticvoidmain(String[] args) {
-        Scannerscanner=newScanner(System.in);
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         try {
-            StringmyInput=scanner.nextLine();
-            String[] myParts=myInput.split(",");
-            if (myParts.length!=4) {
-                thrownewIllegalArgumentException("Invalid Input");
-            }
-            
-            StringmyName=myParts[0];
-            intmyBirthMonth=Integer.parseInt(myParts[1]);
-            intmyBirthDay=Integer.parseInt(myParts[2]);
-            intmyBirthYear=Integer.parseInt(myParts[3]);
-            
-            if (myBirthMonth<1||myBirthMonth>12||myBirthDay<1||myBirthDay>31||myBirthYear<1950||myBirthYear>2024) {
-                thrownewIllegalArgumentException("Invalid Input");
-            }
+            // Input for your birthday
+            String myInput = scanner.nextLine();
+            String[] myData = myInput.split(",");
+            if (myData.length != 4) throw new IllegalArgumentException("Invalid Input");
+            String myName = myData[0].trim();
+            int myMonth = Integer.parseInt(myData[1].trim());
+            int myDay = Integer.parseInt(myData[2].trim());
+            int myYear = Integer.parseInt(myData[3].trim());
+            if (myMonth < 1 || myMonth > 12 || myDay < 1 || myDay > 31 || myYear < 1950 || myYear > 2024) throw new IllegalArgumentException("Invalid Input");
 
-            StringfriendInput=scanner.nextLine();
-            String[] friendParts=friendInput.split(",");
-            if (friendParts.length!=4) {
-                thrownewIllegalArgumentException("Invalid Input");
-            }
+            // Input for friend's birthday
+            String friendInput = scanner.nextLine();
+            String[] friendData = friendInput.split(",");
+            if (friendData.length != 4) throw new IllegalArgumentException("Invalid Input");
+            String friendName = friendData[0].trim();
+            int friendMonth = Integer.parseInt(friendData[1].trim());
+            int friendDay = Integer.parseInt(friendData[2].trim());
+            int friendYear = Integer.parseInt(friendData[3].trim());
+            if (friendMonth < 1 || friendMonth > 12 || friendDay < 1 || friendDay > 31 || friendYear < 1950 || friendYear > 2024) throw new IllegalArgumentException("Invalid Input");
 
-            StringfriendName=friendParts[0];
-            intfriendBirthMonth=Integer.parseInt(friendParts[1]);
-            intfriendBirthDay=Integer.parseInt(friendParts[2]);
-            intfriendBirthYear=Integer.parseInt(friendParts[3]);
-            
-            if (friendBirthMonth<1||friendBirthMonth>12||friendBirthDay<1||friendBirthDay>31||friendBirthYear<1950||friendBirthYear>2024) {
-                thrownewIllegalArgumentException("Invalid Input");
-            }
+            // Create BirthDay objects
+            BirthDay myBirthday = new BirthDay(myName, myMonth, myDay, myYear);
+            BirthDay friendBirthday = new BirthDay(friendName, friendMonth, friendDay, friendYear);
 
-            BirthDaymyBirthday=newBirthDay(myName, myBirthMonth, myBirthDay, myBirthYear);
-            BirthDayfriendBirthday=newBirthDay(friendName, friendBirthMonth, friendBirthDay, friendBirthYear);
-
-            System.out.println("In the USA my birthday is: "+myBirthday.getUsaFormat());
-            System.out.println("In England my birthday is: "+myBirthday.getUkFormat());
-            System.out.println("In the USA my friend "+friendName+"'s birthday is: "+friendBirthday.getUsaFormat());
-            System.out.println("In England my friend "+friendName+"'s birthday is: "+friendBirthday.getUkFormat());
-            
-        } catch (Exceptione) {
+            // Print birthdays
+            myBirthday.printBirthday(false);
+            friendBirthday.printBirthday(true);
+        } catch (Exception e) {
             System.out.println("Invalid Input");
         } finally {
             scanner.close();
